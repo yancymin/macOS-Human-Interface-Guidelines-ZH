6cc7a985455628cc2d14a8f41631006e85aa29ca
yancymin <senseslofter@gmail.com>
2019-01-26 16:26:40 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: '',
+    description: 'Ejgkjbjbj', //描述
+    dest: './dist',   // 设置输出目录
+    port: 8080, //端口
+    localePath: '/docs/REMADE2.html',
+    themeConfig: { //主题配置
+        logo: '/imgs/apple-developer-brandmark.svg',
+    }
+}
+  
