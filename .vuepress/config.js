64303a1ada9a59fee4d41bba771ef32da7a17287
yancymin <senseslofter@gmail.com>
2019-02-11 16:07:07 +0800

    -

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,16 +7,16 @@ module.exports = {
         title: 'macOS',
         collapsable: true,
         children: [
-          ['/1-macOS/Themes', 'Themes'],
-          ['/1-macOS/VisualIndex', 'Visual Index']
+          ['docs/1-macOS/Themes', 'Themes'],
+          ['docs/1-macOS/VisualIndex', 'Visual Index']
         ]
       },
       {
         title: 'App Architecture',
         children: [
-          ['/2-App Architecture/Full-ScreenMode', 'Full-Screen Mode'],
-          ['/2-App Architecture/Loading', 'Loading'],
-          ['/2-App Architecture/Modality', 'Modality'],
+          ['docs/2-App Architecture/Full-ScreenMode', 'Full-Screen Mode'],
+          ['docs/2-App Architecture/Loading', 'Loading'],
+          ['docs/2-App Architecture/Modality', 'Modality'],
         ]
       }
     ],
@@ -43,7 +43,7 @@ module.exports = {
     // 假如你的文档仓库和项目本身不在一个仓库：
     // docsRepo: 'vuejs/vuepress',
     // 假如文档不是放在仓库的根目录下：
-    // docsDir: 'docs',
+    docsDir: 'docs',
     // 假如文档放在一个特定的分支下：
     docsBranch: 'master',
     // 默认是 false, 设置为 true 来启用
